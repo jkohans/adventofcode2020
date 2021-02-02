9c2f6c6da78316d6b0eda224712f4ded4654f1aa
Jeremy Kohansimeh <jeremy@flatiron.com>
2021-02-02 17:27:09 -0500

    day 23 part 2 solution - convert to linked-list impl

--- day23/day23.py
+++ day23/day23.py
@@ -1,82 +1,119 @@
-def pick3_cards(cups, index):
-    print(cups)
+def pick3_ll(right_pointers, current_cup):
+    pick3 = []
+    next_ = right_pointers[current_cup]
 
-    # pick up three cups immediately clockwise of current and remove
-    #  if lower than any cup then wraps around to highest cup label instead
-    start = index + 1
-    end = index + 4
+    for _ in range(3):
+        pick3.append(next_)
+        next_ = right_pointers[next_]
 
-    if start > len(cups):  # on last item
-        start = start % len(cups)  # should always be zero
-        end = end % len(cups)
-        pick3 = cups[start:end]
-        cups = cups[end:]
-    elif end > len(cups):  # need some from end, and some from beginning
-        pick3 = cups[start:]
-        remainder = 3 - len(pick3)
-        pick3 += cups[:remainder]
+    # chop out the picked list
+    right_pointers[current_cup] = next_
 
-        cups = cups[remainder:start]
-    else:  # all in the middle
-        pick3 = cups[start:end]
+    for label in pick3:
+        del right_pointers[label]
 
-        cups = cups[:start] + cups[end:]
-
-    print("pick3", pick3)
-    return pick3, cups
+    return pick3, right_pointers
 
 
-def get_destination_index(cups, current_cup):
-    # destination cup = cup w/current label - 1, if above keep subtracting 1 until not.
+def get_destination_ll(right_pointers, current_cup, min_, max_):
+    # need an efficient way to find min and max here
     target = current_cup - 1
-    target_index = -1
 
-    while target not in cups:
-        if target < min(cups):
-            target = max(cups)
-            target_index = cups.index(target)
+    while target not in right_pointers:
+        if target < min_:
+            target = max_
             break
 
         target = target - 1
 
-    if target_index == -1:
-        target_index = cups.index(target)
-
-    return target_index
+    return target
 
 
-def shift_to_target(cups, current_cup, destination_index):
-    next_cups = cups.copy()
+def get_clockwise_order_after1_ll(right_pointers):
+    labels = []
+    ptr = right_pointers[1]
 
-    while next_cups[destination_index] != current_cup:
-        last_item = len(next_cups) - 1  # rotate last item to the front
-        next_cups = [next_cups[last_item]] + next_cups[:last_item]
+    while ptr != 1:
+        labels.append(ptr)
+        ptr = right_pointers[ptr]
 
-    return next_cups
+    return labels
 
 
-def get_clockwise_order_after1(cups):
-    one_index = cups.index(1)
+def initialize_right_pointers(cups):
+    right_pointers = {}
+    prev_cup = None
 
-    rotated = cups[one_index:] + cups[:one_index]
-    return rotated[1:]
+    for cup in cups:
+        if not prev_cup:  # first element
+            prev_cup = cup
+        else:
+            right_pointers[prev_cup] = cup
+            prev_cup = cup
+
+    right_pointers[prev_cup] = cups[0]  # complete the circle
+
+    return right_pointers
+
+
+def find_min_max(sorted_cups, pick3):
+    idx_min = 0
+    idx_max = len(sorted_cups) - 1
+    min_, max_ = sorted_cups[idx_min], sorted_cups[idx_max]
+
+    while min_ in pick3:
+        idx_min = idx_min + 1
+        min_ = sorted_cups[idx_min]
+
+    while max_ in pick3:
+        idx_max = idx_max - 1
+        max_ = sorted_cups[idx_max]
+
+    return min_, max_
 
 
 if __name__ == "__main__":
     # cups = [3, 8, 9, 1, 2, 5, 4, 6, 7]
     cups = [3, 9, 8, 2, 5, 4, 7, 1, 6]
+    fill_size = 1000000
+    biggest = max(cups)
+    cups += list(map(lambda x: biggest + x, range(1, fill_size - len(cups) + 1)))  # fill up to 1M
+    sorted_cups = sorted(cups)
 
-    for i in range(100):
-        index = i % len(cups)
-        print("*****")
-        print("move", i + 1)
-        current_cup = cups[index]
-        print("current cup", current_cup)
-        pick3, cups = pick3_cards(cups, index)
-        destination_idx = get_destination_index(cups, current_cup)
-        print("destination", cups[destination_idx])
-        # cups picked up immediately clockwise of destination cup
-        cups = cups[: destination_idx + 1] + pick3 + cups[destination_idx + 1 :]
-        # shift until current_cup is back in position index
-        cups = shift_to_target(cups, current_cup, index)
-    print("finally", "".join(map(str, get_clockwise_order_after1(cups))))
+    right_pointers = initialize_right_pointers(cups)
+
+    # part 2:
+    # - one million cups! remaining cups are max+1, max+2, ... max+n
+    # - 10 million moves!
+    # - find two cups immediately clockwise of cup 1
+
+    i = 0
+    current_cup = cups[0]
+
+    while i < 10000000:
+        # print("*** move", i+1)
+        # print(right_pointers)
+        pick3, right_pointers = pick3_ll(right_pointers, current_cup)
+        # print("pick3", pick3)
+
+        min_, max_ = find_min_max(sorted_cups, pick3)
+
+        # find destination and insert, no need to shift
+        destination = get_destination_ll(right_pointers, current_cup, min_, max_)
+        # print("destination", destination)
+        old_dest_right_ptr = right_pointers[destination]
+
+        for label in pick3:
+            right_pointers[destination] = label
+            destination = label
+
+        right_pointers[pick3[2]] = old_dest_right_ptr
+
+        current_cup = right_pointers[current_cup]
+        i = i + 1
+
+    # print("finally", "".join(map(str, get_clockwise_order_after1_ll(right_pointers))))
+
+    after_one = right_pointers[1]
+    after_after_one = right_pointers[after_one]
+    print(after_one * after_after_one)
